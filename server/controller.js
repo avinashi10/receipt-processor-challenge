9171b65ff8ec0768b835988bf38a2c280981f72e
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-06-19 08:24:54 -0500

    Updated MVC files

--- server/controller.js
+++ server/controller.js
@@ -3,6 +3,11 @@ const receiptModel = require('./model.js');
 
 module.exports = {
   createReceipt: (req, res) => {
-
+    model.setReceipt(req.body)
+      .then(() => res.send('Receipt added'))
+      .catch((err) => res.status(400).send(err));
+  },
+  getReceiptId: (req, res) => {
+    .then()
   }
 }
