bcf67c70b084f41aa32e81fdc1c3aa25d38c7b93
Archaa Work <archaaworks@Archaas-MacBook-Air.local>
2023-06-19 14:13:17 -0500

    server up and running, debugging controllers.js functions

--- server/controller.js
+++ server/controller.js
@@ -4,19 +4,19 @@ const calculatePoints = require('./points.js');
 
 module.exports = {
   createReceipt: (req, res) => {
-    model.setReceipt(req.body)
+    receiptModel.setReceipt(req.body)
       .then((receiptId) => res.json({ id: receiptId }))
       .catch((err) => res.status(400).send(err));
   },
   getPoints: (req, res) => {
-    model.findReceiptById(req.params)
-      .then(receiptData) => {
+    receiptModel.findReceiptById(req.params)
+      .then((receiptData) => {
         // Calculate points based on receipt data
         const points = calculatePoints(receiptData);
         res.json({ points: points });
-      }
-      .catch(err) => {
+      })
+      .catch((err) => {
         res.status(404).send("No receipt found for that id: ", err)
-      }
+      })
   }
 }
